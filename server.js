5097511289bcdfa1ef913233db78a137df9da889
Pooja <poojaecstatic@gmail.com>
2017-02-10 18:57:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -25,15 +25,56 @@ var articleOne=
                  This is the content for my first article. This is the content for my first article. This is the content for my first article.
                   This is the content for my first article. This is the content for my first article. This is the content for my first articl.
             </p> `
-}
+};
 
+function createTemplate(data)
+{
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.heading;
+    var content=data.content;
+var htmlTemplate=`
+<html>
+    <head>
+         <link href="/ui/style.css" rel="stylesheet" />
+        <title>
+           ${title}
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <style>
+            
+        </style>
+    </head>
+    
+    <body>
+        <div class="container">
+        <div>
+            <a href="/">Home</a>
+            
+        </div>
+        <hr>
+        <h3>
+        ${heading}
+        </h3>
+        <div>
+            ${date}
+        </div>
+        <div>
+           ${content}
+        </div>
+        </div>
+    </body>
+</html>
+`;
+return htmlTemplate;
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two', function (req, res) {
