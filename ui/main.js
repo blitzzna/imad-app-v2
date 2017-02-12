3320fc355c15166054156016961497928317d130
Pooja <poojaecstatic@gmail.com>
2017-02-12 22:41:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,23 @@
-console.log('Loaded!');
+//counter code
+var button=document.getElementById('counter');
+
+
+button.onclick=function(){
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function()
+    {
+        if(request.readyState===XMLHttpRequset.DONE)
+        {
+            if(request.status===200)
+            {
+                var counter=request.responseText;
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+        }
+    };
+    
+    request.open('GET','http://blitzzna.imad.hasura-app.io/counter',true);
+    request.send(null);
+    
+};
