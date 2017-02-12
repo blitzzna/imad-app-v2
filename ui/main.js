4a857026e3c5734de94e53183cf501233b4152ce
Pooja <poojaecstatic@gmail.com>
2017-02-12 23:17:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ button.onclick=function(){
     var request=new XMLHttpRequest();
     request.onreadystatechange=function()
     {
-        if(request.readyState===XMLHttpRequset.DONE)
+        if(request.readyState===XMLHttpRequest.DONE)
         {
             if(request.status===200)
             {
