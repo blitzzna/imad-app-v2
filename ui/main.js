34494f4d5d3546e100acc73a3af6f33619c9135b
Pooja <poojaecstatic@gmail.com>
2017-04-11 13:12:28 +0530

    Add files via upload

--- ui/main.js
+++ ui/main.js
@@ -1,23 +1,135 @@
-//counter code
-var button=document.getElementById('counter');
-
-
-button.onclick=function(){
-    var request=new XMLHttpRequest();
-    request.onreadystatechange=function()
+/*var cnter=document.getElementById('click');
+var counter=0;
+cnter.onclick=function(){
+var request=new XMLHttpRequest();
+request.onreadystatechange=function()
+{
+    if(request.readyState==XMLHttpRequest.DONE)
     {
-        if(request.readyState===XMLHttpRequest.DONE)
-        {
-            if(request.status===200)
-            {
-                var counter=request.responseText;
-                var span=document.getElementById('count');
-                span.innerHTML=counter.toString();
-            }
-        }
-    };
-    
-    request.open('GET','http://blitzzna.imad.hasura-app.io/counter',true);
-    request.send(null);
-    
+         if(request.status==200)
+         {
+             console.log(request.responseText);
+          var counter=request.responseText;
+          var cnt2=document.getElementsByClassName('cnt');
+         cnt2[0].innerHTML=counter.toString();
+         }
+    }
 };
+     request.open('GET','http://sattybhatt.imad.hasura-app.io/counter',true);
+    request.send(null);
+};
+var z=document.getElementsByClassName('img-medium');
+z[0].onclick=function(){
+    hello1();
+    setInterval(hello1,2000);
+};
+var marginLefter=0;
+function hello1(){  
+    console.log('hello');
+    marginLefter =marginLefter+2;
+    z[0].style.marginLeft=marginLefter+'px';
+} */
+//code for registering user submit_btn
+var submit_btn=document.getElementById('submitbutton');
+submit_btn.onclick=function(){
+    var lo=document.getElementById('lk');
+    lo.style.display='block';
+    var lc=document.getElementById('mm');
+    lc.style.opacity='0.2';
+    console.log('hello submit button is clicked');
+var request=new XMLHttpRequest();
+request.onreadystatechange=function()
+{
+    if(request.readyState==XMLHttpRequest.DONE)
+    {
+         if(request.status==200)
+         {
+             console.log(request.responseText);
+            alert('USer created');
+         }
+         else if(request.status==403)
+         {
+            alert('USername/password is incorrect');
+         }
+         else if(request.status==500)
+         {
+            alert('Something went wrong on server');
+         }
+    }
+};
+var username2=document.getElementById('usrnm').value;
+var password2=document.getElementById('password').value;
+console.log(username2);
+console.log(password2);
+     request.open('POST','http://sattybhatt.imad.hasura-app.io/create-user',true);
+     request.setRequestHeader('Content-type','application/json');
+     request.send(JSON.stringify({username: username2,password: password2}));
+};
+//end here
+//code for login
+var submit_btn2=document.getElementById('rsubmitbutton');
+submit_btn2.onclick=function(){
+ var lo=document.getElementById('lgif');
+    lo.style.display='block';
+    var lc=document.getElementById('mm');
+    lc.style.opacity='0.2';
+var request=new XMLHttpRequest();
+request.onreadystatechange=function()
+{
+    if(request.readyState==XMLHttpRequest.DONE)
+    {
+         if(request.status==200)
+         {     
+              lo.style.display='none';
+              lc.style.opacity='1.0';
+             var m="";
+             console.log(request.responseText);
+             var x=new Date().getHours();
+             if((x>6)&&(x<12)){ m="Good mroning "+username;}
+             else if((x>12)&&(x<22)){ m="Good evening "+username;}
+             else{ m="Good evening "+username;}
+             var n2=document.getElementById('gm');
+             var n3=document.getElementById('lgin');
+             n3.style.display='none';
+             n2.innerHTML=m;
+             n2.style.display='inline-block';
+            //alert(m);
+         }
+         else if(request.status==403)
+         {
+            alert('USername/password is incorrect');
+         }
+         else if(request.status==500)
+         {
+            alert('Something went wrong on server');
+         }
+    }
+};
+var username=document.getElementById('rusername').value;
+var password=document.getElementById('rpassword').value;
+console.log(username);
+console.log(password);
+     request.open('POST','http://sattybhatt.imad.hasura-app.io/login',true);
+     request.setRequestHeader('Content-type','application/json');
+    request.send(JSON.stringify({username:username,password:password}));
+};
+//end here
+  var lo2=document.getElementById('addo');
+  lo2.onclick=function(){
+      var lo3=document.getElementById('lk');
+     lo3.style.display='none'; 
+      var lc=document.getElementById('mm');
+    lc.style.opacity='1.0';
+  };
+  //logout
+  var logout=document.getElementById('logout');
+  logout.onclick=function(){
+      var gm=document.getElementsByClassName('gm');
+      gm[1].style.display='none';
+      var lgin=document.getElementById('lgin');
+      lgin.style.display='inline';
+      var logout=document.getElementById('logout');
+      logout.style.display='none';
+  };
+ 
+    
---
+++ ui/main2.js
@@ -0,0 +1,50 @@
+
+var xr=document.getElementById('lcarea');
+xr.onclick=function(){
+var zr=document.getElementById('writeres');
+zr.style.display='block';
+};
+//
+//comment
+var submit_btn3=document.getElementById('submitc');
+submit_btn3.onclick=function(){
+    console.log('hello submit button is clicked');
+var request=new XMLHttpRequest();
+request.onreadystatechange=function()
+{
+    if(request.readyState==XMLHttpRequest.DONE)
+    {
+         if(request.status==200)
+         {
+         }
+         else if(request.status==403)
+         {
+         }
+         else if(request.status==500)
+         {
+         }
+    }
+};
+var comment=document.getElementById('comment').innerHTML;
+var user=document.getElementById('username2').value;
+           var commer ='<div style="padding: 15px;position: relative;min-height: 50px;background: rgba(204, 204, 204, 0.07);border-radius: 5px;border: 1px solid #f5f5f5;box-shadow: 1px 1px 6px -3px #7d7d7d;margin-bottom: 20px;"><div style="position: absolute; height: 50px; width: 50px; border-radius: 50%; top:15px; left:15px; overflow: hidden;"><img src="http://i2.wp.com/www.wallpapersbyte.com/wp-content/uploads/2015/06/Space-Galaxy-Circular-Nebula-Vortex-Stars-In-Dark-Blue-Color-WallpapersByte-com-1366x768.jpg?resize=1024%2C576" style="width: 100%; height: 100%;"></div><div style="width:calc(100% - 50px); margin-left: 65px;"><div style="font-weight: bold; ">'+user+'</div><div style="margin-top: 7px;">'+comment+'</div></div></div>';
+            var z2=document.getElementById('lcm');
+            z2.innerHTML =commer+z2.innerHTML ;
+            //
+var aid=document.getElementById('aid').value;
+//console.log(comment);
+     request.open('POST','http://sattybhatt.imad.hasura-app.io/comment',true);
+     request.setRequestHeader('Content-type','application/json');
+    request.send(JSON.stringify({comment1:comment,aid2:aid}));
+};
+//end here
+ //logout
+  var logout=document.getElementById('logout');
+  logout.onclick=function(){
+      var gm=document.getElementsByClassName('gm');
+      gm[1].style.display='none';
+      var lgin=document.getElementById('lgin');
+      lgin.style.display='inline';
+      var logout=document.getElementById('logout');
+      logout.style.display='none';
+  };
